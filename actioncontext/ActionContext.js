cda3d8546f0033ef4d507fedd7df26350d132ac0
Staffan Olsson <staffan@repos.se>
2015-01-24 13:26:15 +0100

    An idea on how to make a function both a constructor and a mixin, allowing composable behavior instead of the dreaded extend.

--- actioncontext/ActionContext.js
+++ actioncontext/ActionContext.js
@@ -1,7 +1,8 @@
-module.exports = function () {
-  var o = this;
-
-  var attributes = {};
+module.exports = function(o) {
+  // work as both constructor and mixin
+  o || (o = this);
+  // use private state unless mixin to object that declares public state
+  var attributes = o.attributes || {};
 
   o.set = function(key, value) {
     attributes[key] = value;
