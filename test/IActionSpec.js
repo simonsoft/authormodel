8208432d2f4e5f7757108385ae6335388048c354
Staffan Olsson <staffan@repos.se>
2015-02-12 15:23:46 +0100

    Action menu can now add new units with uniteditors (to btguidline demo).
    Adds additional specs on unit editors, some of which we log warnings on in editor.
    Actions are now independent Command pattern objects that are typically added to context,
    from any interested party such as the originating unit editor, the content model or the editor.
    Issue: enter key press doesn't work in new editors.

--- test/IActionSpec.js
+++ test/IActionSpec.js
@@ -1,71 +1,67 @@
 
 module.exports = function interfaceTest(impl, required) {
 
-  describe("!Action: " + impl, function() {
+  describe("Is a Command pattern handle passed through ActionContext to UI that might trigger actions", function() {
 
-    describe("Is a Command pattern handle passed through ActionContext to UI that might trigger actions", function() {
+  });
 
-    });
+  describe("#identify", function() {
 
-    describe("#identify", function() {
-
-      it("Returns a descriptor object", function() {
-
-      });
-
-      it(".key is a dot notation style identifier for the operation label", function() {
-
-      });
-
-      it(".attributes, if defined, has variables used to render label", function() {
-
-      });
+    it("Returns a descriptor object", function() {
 
     });
 
-    describe("#execute", function() {
-
-      it("1st arg is the actionContext", function() {
-
-      });
-
-      it("2nd arg is the authoringCollection", function() {
-
-      });
+    it(".key is a dot notation style identifier for the operation label", function() {
 
     });
 
-    describe("#preview", function() {
+    it(".attributes, if defined, has variables used to render label", function() {
 
-      it("1st arg is the actionContext", function() {
+    });
 
-      });
+  });
 
-      it("2nd arg is the authoringCollection", function() {
+  describe("#execute", function() {
 
-      });
+    it("1st arg is the actionContext", function() {
 
-      it("Modified the authoringCollection", function() {
+    });
 
-      });
+    it("2nd arg is the authoringCollection", function() {
 
-      xit("Sets isPreview on added models, but what does it do to modify?", function() {
+    });
 
-      });
+  });
 
-      xit("Do we have a transaction+rollback concept, or do w")
+  describe("#preview", function() {
 
-      it("Returns a *preview* object", function() {
+    it("1st arg is the actionContext", function() {
 
-      });
+    });
 
-      it("The *preview* object has an #execute method", function() {
+    it("2nd arg is the authoringCollection", function() {
 
-      });
+    });
 
-      it("The *preview* object's #cancel method, if defined, _must_ be invoked if the action is aborted", function() {
+    it("Modified the authoringCollection", function() {
 
-      });
+    });
+
+    xit("Sets isPreview on added models, but what does it do to modify?", function() {
+
+    });
+
+    xit("Do we have a transaction+rollback concept, or do w")
+
+    it("Returns a *preview* object", function() {
+
+    });
+
+    it("The *preview* object has an #execute method", function() {
+
+    });
+
+    it("The *preview* object's #cancel method, if defined, _must_ be invoked if the action is aborted", function() {
 
     });
 
