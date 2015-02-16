5c70fc50a22aba27e8ad961721baccb4ecd9be2b
Staffan Olsson <solsson@gmail.com>
2015-02-16 08:37:47 +0100

    Mixin adds disband behavior to ImageEditorView. I was about to add it to the superclass first, but once again I realized that it has no merit to bundle every common behavior into the same superclass.

--- test/AuthoringRenderEngineSpec.js
+++ test/AuthoringRenderEngineSpec.js
@@ -4,14 +4,45 @@ var expect = require('chai').expect;
 
 module.exports = function interfaceSpec(required) {
 
-  describe("Constructor", function() {
+  describe("Handle unit add", function() {
 
-    it("is a constructor", function() {
-      expect(required).to.be.a('function');
-      expect(required.name).to.equal('AuthoringRenderEngine');
-      // instances
-      //expect(required.constructor).to.exist();
-      //expect(required.constructor.name).to.equal('AuthoringRenderEngine');
+  });
+
+  describe("Handle unit remove (actual remove, not just flagged)", function() {
+
+  });
+
+  describe("Handle unit change", function() {
+
+    xit("EITHER expects unit editor to render automatically", function() {
+    });
+    xit("OR calls render on unit editor", function() {
+    });
+
+  });
+
+  describe("handle unit editor 'changed'", function() {
+
+    it("Who calls save?");
+
+  });
+
+  describe("Events from unit editors", function() {
+
+    xit("Re-emits any unrecognized", function() {
+
+    });
+
+    xit("Re-emits authorintent", function() {
+
+    });
+
+    xit("Re-emits authorpause", function() {
+
+    });
+
+    xit("Given the 'disband' event it requests a new unit editor from content model", function() {
+
     });
 
   });
